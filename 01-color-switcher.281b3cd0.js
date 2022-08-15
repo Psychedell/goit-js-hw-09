7a70cb909df470ec70cde0063396b3341a107ec0
Psychedell <100085494+Psychedell@users.noreply.github.com>
2022-08-15 13:11:40 +0000

    Deploying to gh-pages from @ Psychedell/goit-js-hw-09@94c7c58acabed05db90ce87a89336ab9e62041b0 🚀

---
+++ 01-color-switcher.281b3cd0.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");function n(){return r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){timerId=setInterval(n,1e3),t.setAttribute("disabled","disabled")})),e.addEventListener("click",(function(){clearInterval(timerId),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.281b3cd0.js.map
