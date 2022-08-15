94c7c58acabed05db90ce87a89336ab9e62041b0
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-08-15 16:11:16 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+start.addEventListener('click', onBtnStartColorChanger);
+stop.addEventListener('click', onBtnStopColoChangerStop);
+
+function onBtnStartColorChanger() {
+  timerId = setInterval(getRandomHexColor, 1000);
+  start.setAttribute('disabled', 'disabled');
+}
+
+function onBtnStopColoChangerStop() {
+  clearInterval(timerId);
+  start.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return (body.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`);
+}
