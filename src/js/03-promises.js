09d3b70eb05e62efc2c607ae4f71763ad07c4b7e
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-08-18 12:06:59 +0300

    task 3 edited

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -33,8 +33,14 @@ function addPromiseNotify({ amount, delay, step }) {
   let delayTime = delay;
   for (let i = 1; i <= amount; i += 1) {
     createPromise(i, delayTime)
-      .then(value => Notiflix.Notify.success(value))
-      .catch(error => Notiflix.Notify.failure(error));
+      .then(({ position, delay }) =>
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        )
+      )
+      .catch(({ position, delay }) =>
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
 
     delayTime += step;
   }
